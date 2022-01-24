2981a0966baeb7b3e8d61f813ccbfbdc2e7f7468
GandenFung <97749668+GandenFung@users.noreply.github.com>
2022-01-23 22:43:32 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$(".stats").on("click",changeColor);
+
+function changeColor(){
+	document.body.style.background = "orange";
+    alert("Thanks for Caring About Our Success!");
+
+}
+$(".mainPhoto").on("click",clicked);
+
+function clicked(){
+	
+    alert("Bardbarians playing Stuy 2021");
+
+}
