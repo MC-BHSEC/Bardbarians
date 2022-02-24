c0b7ddcf7c28d5262310db996db70220d85bbc3d
MC-BHSEC <80209223+MC-BHSEC@users.noreply.github.com>
2022-02-24 12:20:30 -0500

    Add files via upload

---
+++ roster.js
@@ -0,0 +1,6 @@
+$(".pic1").on("click",playerOneBio);
+
+function playerOneBio(){
+	alert("Ganden plays on the powerline as a cutter who can also play as a handler after Jack, Nadav, and Dexter. He has played on the team since 9th grade but was injured during states so his senior year will be his first time playing in teh state championsgip tournament. In his free time he plays video games and also plays the piano.");
+
+}
